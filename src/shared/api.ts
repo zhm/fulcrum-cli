24e5268897c212eb96615c68999e7b05a2c868ae
Zac McCormick <zac.mccormick@gmail.com>
2022-07-14 11:13:55 -0400

    More complete setup of variables

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import {
-  Form, Record, RepeatableItemValue, RepeatableValue, Feature,
+  Form, Record, RepeatableItemValue, RepeatableValue, Feature, DateUtils,
 } from 'fulcrum-core';
-import ContainerElement from 'fulcrum-core/src/elements/container-element';
 import fs from 'fs';
 import path from 'path';
 import Sandbox from 'v8-sandbox';
@@ -32,7 +31,6 @@ export async function fetchRecordsBySQL(client: Client, form: Form, sql: string)
   const records = [];
 
   for (const id of ids) {
-    // eslint-disable-next-line no-await-in-loop
     records.push(new Record(await client.records.find(id), form));
   }
 
@@ -55,7 +53,7 @@ export async function updateCalculatedFields(record: Record) {
   try {
     sandbox = new Sandbox({ template: EXPRESSIONS });
 
-    await updateCalculatedFieldsRecursive(sandbox, record, record, record.form);
+    await updateCalculatedFieldsRecursive(sandbox, record, record);
   } finally {
     if (sandbox) {
       await sandbox.shutdown();
@@ -63,52 +61,17 @@ export async function updateCalculatedFields(record: Record) {
   }
 }
 
-export async function updateCalculatedFieldsRecursive(sandbox: Sandbox, record: Record, feature: Feature, container: ContainerElement) {
+export async function updateCalculatedFieldsRecursive(sandbox: Sandbox, record: Record, feature: Feature) {
+  const repeatable = feature instanceof RepeatableItemValue ? feature.element : null;
+  const container = repeatable ?? record.form;
+
   const expressions = container.elementsOfType('CalculatedField', false).map(((field) => ({
     key: field.key,
     dataName: field.dataName,
     expression: field.expression,
   })));
 
-  const runtimeVariables = {
-    expressions,
-    form: record.form.toJSON(),
-    values: record.formValues.toJSON(),
-
-    locale: 'en-US',
-    language: 'en-US',
-    timeZone: null,
-    decimalSeparator: '.',
-    groupingSeparator: ',',
-    currencySymbol: '$',
-    currencyCode: 'USD',
-    country: 'US',
-    deviceIdentifier: null,
-    deviceModel: null,
-    deviceManufacturer: null,
-    platform: 'cli',
-    platformVersion: '1.0.0',
-    application: 'Fulcrum CLI',
-    applicationVersion: '1.0.0',
-    applicationBuild: '',
-    userEmail: null,
-    userRoleName: null,
-    recordStatus: record.status,
-    recordSystemCreatedAt: record.createdAt,
-    recordSystemUpdatedAt: record.updatedAt,
-    recordClientCreatedAt: record.clientCreatedAt,
-    recordClientUpdatedAt: record.clientUpdatedAt,
-    recordProject: record.projectID,
-    recordProjectName: null,
-    recordGeometry: null,
-    recordAltitude: record.altitude,
-    // recordVerticalAccuracy: data.recordVerticalAccuracy,
-    // recordHorizontalAccuracy: data.recordHorizontalAccuracy,
-    featureCreatedAt: null, // item.createdAt,
-    featureUpdatedAt: null, // item.updatedAt,
-    featureGeometry: null,
-    featureIndex: feature.index,
-  };
+  const runtimeVariables = getFeatureVariables(record, feature, {});
 
   const { value, error } = await sandbox.execute({
     code: SCRIPT,
@@ -131,8 +94,119 @@ export async function updateCalculatedFieldsRecursive(sandbox: Sandbox, record: 
   for (const formValue of feature.formValues.all) {
     if (formValue instanceof RepeatableValue) {
       for (const item of formValue.items) {
-        await updateCalculatedFieldsRecursive(sandbox, record, item, formValue.element);
+        await updateCalculatedFieldsRecursive(sandbox, record, item);
       }
     }
   }
+}
+
+interface ExpressionEnvironment {
+  locale?: string;
+  language?: string;
+  timeZone?: string;
+  decimalSeparator?: string;
+  groupingSeparator?: string;
+  currencySymbol?: string;
+  currencyCode?: string;
+  country?: string;
+  deviceIdentifier?: string;
+  deviceModel?: string;
+  deviceManufacturer?: string;
+  platform?: string;
+  platformVersion?: string;
+  application?: string;
+  applicationVersion?: string;
+  applicationBuild?: string;
+}
+
+const DEFAULT_ENVIRONMENT: ExpressionEnvironment = {
+  locale: 'en_US',
+  language: 'en-US',
+  timeZone: 'America/New_York',
+  decimalSeparator: '.',
+  groupingSeparator: ',',
+  currencySymbol: '$',
+  currencyCode: 'USD',
+  country: 'US',
+  deviceIdentifier: 'Unknown',
+  deviceModel: 'Unknown',
+  deviceManufacturer: 'Unknown',
+  platform: 'cli',
+  platformVersion: '1.0.0',
+  application: 'Fulcrum CLI',
+  applicationVersion: '1.0.0',
+  applicationBuild: '',
+};
+
+function getFeatureVariables(record: Record, feature: Feature, environment: ExpressionEnvironment) {
+  const repeatable = feature instanceof RepeatableItemValue ? feature.element : null;
+  const container = repeatable ?? record.form;
+
+  const expressions = container.elementsOfType('CalculatedField', false).map(((field) => ({
+    key: field.key,
+    dataName: field.dataName,
+    expression: field.expression,
+  })));
+
+  return {
+    ...DEFAULT_ENVIRONMENT,
+    ...environment,
+    expressions,
+    form: record.form.toJSON(),
+    values: record.formValues.toJSON(),
+    repeatable: repeatable?.key ?? null,
+
+    userEmail: null,
+    userFullName: null,
+    userRoleName: null,
+
+    recordID: record.id,
+    recordStatus: record.status,
+    recordClientCreatedAt: record.clientCreatedAt,
+    recordClientUpdatedAt: record.clientUpdatedAt,
+    recordProject: record.projectID,
+    recordProjectName: null,
+    recordGeometry: record.geometryAsGeoJSON,
+
+    recordAltitude: record.altitude,
+    recordVerticalAccuracy: record.recordVerticalAccuracy,
+    recordHorizontalAccuracy: record.recordHorizontalAccuracy,
+
+    recordCreatedLatitude: record.createdLatitude,
+    recordCreatedLongitude: record.createdLongitude,
+    recordCreatedAltitude: record.createdAltitude,
+    recordCreatedAccuracy: record.createdAccuracy,
+
+    recordUpdatedLatitude: record.updatedLatitude,
+    recordUpdatedLongitude: record.updatedLongitude,
+    recordUpdatedAltitude: record.updatedAltitude,
+    recordUpdatedAccuracy: record.updatedAccuracy,
+
+    recordCreatedDuration: record.createdDuration,
+    recordUpdatedDuration: record.updatedDuration,
+    recordEditedDuration: record.editedDuration,
+
+    featureID: feature.id,
+    featureIndex: feature.index,
+    featureIsNew: feature.id == null,
+    featureCreatedAt: DateUtils.formatEpochTimestamp(feature.createdAt),
+    featureUpdatedAt: DateUtils.formatEpochTimestamp(feature.updatedAt),
+    featureGeometry: feature.geometryAsGeoJSON,
+
+    featureCreatedLatitude: feature.createdLatitude,
+    featureCreatedLongitude: feature.createdLongitude,
+    featureCreatedAltitude: feature.createdAltitude,
+    featureCreatedAccuracy: feature.createdAccuracy,
+
+    featureUpdatedLatitude: feature.updatedLatitude,
+    featureUpdatedLongitude: feature.updatedLongitude,
+    featureUpdatedAltitude: feature.updatedAltitude,
+    featureUpdatedAccuracy: feature.updatedAccuracy,
+
+    featureCreatedDuration: feature.createdDuration,
+    featureUpdatedDuration: feature.updatedDuration,
+    featureEditedDuration: feature.editedDuration,
+
+    currentLocation: null,
+  };
 }
